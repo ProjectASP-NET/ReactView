4535169f20c9651c95474a47b0f13163c74e838e
Dimass-dev <dmitrii.olaresco@isa.utm.md>
2026-03-23 22:16:31 +0200

    Буду пробовать на NExtJS

--- src/components/Counter.tsx
+++
@@ -1,5 +0,0 @@
-export default function Counter({count = 0}:{count?: number}){
-  return (
-    <div className="counter">🛒 {count}</div>
-  )
-}
--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-  return (
-    <footer className="site-footer">
-      <div>© {new Date().getFullYear()} Vape Store</div>
-    </footer>
-  )
-}
