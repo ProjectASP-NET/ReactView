3584284f40c630bfe19adc5f7cbc638f46626f89
Dimass-dev <dmitrii.olaresco@isa.utm.md>
2026-03-23 23:31:04 +0200

    внес начальные поправки

---
+++ catalog/Products.ts
@@ -0,0 +1,28 @@
+import { Product } from "../types/Mockdata";
+
+export const MOCK_PRODUCTS: Product[] = [
+  {
+    id: "1",
+    type: "vape",
+    name: "XROS 3 Mini",
+    brand: "Vaporesso",
+    price: 1200,
+    img: "/images/products/xros.png",
+    description: "Vape",
+    batteryCapacity: 1000,
+    maxPower: 16,
+    color: "Space Grey"
+  },
+  {
+    id: "2",
+    type: "liquid",
+    name: "Husky Salt",
+    brand: "Voodoo Lab",
+    price: 450,
+    img: "/images/products/husky.png",
+    description: "IceLiquid",
+    volume: 30,
+    nicotine: 20,
+    flavor: ["Ice", "WaterMelon"]
+  },
+];
---
+++ types/Mockdata.ts
@@ -0,0 +1,24 @@
+export interface IProductProps{
+    id: string;
+    name: string;
+    price: number;
+    img : string;
+    brand? : string;
+    description? : string;  
+}
+export interface ILiquid extends IProductProps{
+    type : 'liquid';
+    volume: 10 | 30 | 50 | 100;
+    flavor : string[];
+    nicotine : number;
+}
+export interface IVape extends IProductProps{
+    type : 'vape'
+    batteryCapacity: number;    
+    maxPower: number;          
+    color: string;
+}
+export interface IConsumables extends IProductProps{
+    type : 'consumables'
+}
+export  type Product = ILiquid | IVape | IConsumables;
