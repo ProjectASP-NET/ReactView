3a24afc1b411f93d6cdb5a01c0711bb7ecfa8a68
Dimass-dev <dmitrii.olaresco@isa.utm.md>
2026-03-30 18:12:29 +0300

    делаю страницу с возможностью сравнить товары

---
+++ src/app/(publicpages)/matcher/page.tsx
@@ -0,0 +1,3 @@
+export default function Mathcer(){
+<div>Hello World</div>
+}
--- src/config/pages.config.ts
+++ src/config/pages.config.ts
@@ -2,4 +2,5 @@ export const PAGES ={
     HOME : '/',
     CATALOG : '/catalog',
     CART : '/cart',
+    MATHCER : '/mathcer',
 }
