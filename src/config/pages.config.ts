7648b0bb16f8992817fc6d952f6300da6a72af1f
Dimass-dev <dmitrii.olaresco@isa.utm.md>
2026-04-02 23:32:15 +0300

    делаю вход и регестарацию

--- src/config/pages.config.ts
+++ src/config/pages.config.ts
@@ -4,6 +4,7 @@ export const PAGES = {
     CART: "/cart",
     MATCHER: "/matcher",
     ABOUT: "/about",
+    AUTH:"/auth",
 
     getProduct: (id: string) => `/catalog/${id}`,
     getCatalog: (sort?: string) => sort ? `/catalog?sort=${sort}` : "/catalog",
